b66f7380dec5da8b4b77c3fd1181d7d17bafd5db
varun <Varun18June@gmail.com>
2025-05-28 17:54:53 +0530

    Genrated Migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250523_080359 from './20250523_080359';
+import * as migration_20250528_122406 from './20250528_122406';
 
 export const migrations = [
   {
     up: migration_20250523_080359.up,
     down: migration_20250523_080359.down,
-    name: '20250523_080359'
+    name: '20250523_080359',
+  },
+  {
+    up: migration_20250528_122406.up,
+    down: migration_20250528_122406.down,
+    name: '20250528_122406'
   },
 ];

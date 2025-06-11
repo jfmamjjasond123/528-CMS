277b299d22cc6a99d26dd84f401079d8d633ed99
varun <Varun18June@gmail.com>
2025-06-11 18:27:04 +0530

    minor migration fix

--- src/migrations/20250611_093329.ts
+++ src/migrations/20250611_093329.ts
@@ -2,10 +2,14 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
-   ALTER TABLE "payload_cms"."passage_questions" ADD COLUMN "question_explanation" jsonb;`)
+    ALTER TABLE "payload_cms"."passage_questions"
+    ADD COLUMN IF NOT EXISTS "question_explanation" jsonb;
+  `)
 }
 
 export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
   await db.execute(sql`
-   ALTER TABLE "payload_cms"."passage_questions" DROP COLUMN IF EXISTS "question_explanation";`)
+    ALTER TABLE "payload_cms"."passage_questions"
+    DROP COLUMN IF EXISTS "question_explanation";
+  `)
 }

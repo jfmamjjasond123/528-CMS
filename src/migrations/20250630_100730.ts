6b30cf6b2a9d7deddeaf312d4637b0c2961390ce
varun <Varun18June@gmail.com>
2025-06-30 16:42:50 +0530

    Changes in Package.json

--- src/migrations/20250630_100730.ts
+++ src/migrations/20250630_100730.ts
@@ -1,26 +1,11 @@
 import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
 
-export async function up({ db }: MigrateUpArgs): Promise<void> {
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
-    DO $$
-    BEGIN
-      IF NOT EXISTS (
-        SELECT 1 FROM information_schema.columns
-        WHERE table_schema = 'payload_cms'
-        AND table_name = 'passage_questions_options'
-        AND column_name = 'option_explanation'
-      ) THEN
-        ALTER TABLE "payload_cms"."passage_questions_options"
-        ADD COLUMN "option_explanation" varchar;
-      END IF;
-    END
-    $$;
-  `)
+   ALTER TABLE "payload_cms"."passage_questions_options" ADD COLUMN "option_explanation" varchar;`)
 }
 
-export async function down({ db }: MigrateDownArgs): Promise<void> {
+export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
   await db.execute(sql`
-    ALTER TABLE "payload_cms"."passage_questions_options"
-    DROP COLUMN IF EXISTS "option_explanation";
-  `)
+   ALTER TABLE "payload_cms"."passage_questions_options" DROP COLUMN IF EXISTS "option_explanation";`)
 }
